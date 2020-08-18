2c33108f48c6715e9bde0aeeec133031ac601f30
chris <heldeo3@hotmail.com>
2020-08-18 15:36:27 -0500

    second test case functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[derive(Debug, PartialEq)]
+use std::collections::HashMap;
+#[derive(Debug, PartialEq, Clone)]
 enum TokenType {
     ILLEGAL,
     EOF,
@@ -30,17 +31,35 @@ struct Lexer {
     ch: char,
 }
 
-fn cons_token(token_kind: TokenType, lit: char) -> Token {
+fn cons_token(token_kind: TokenType, lit: String) -> Token {
     Token {
         kind: token_kind,
-        literal: String::from(lit.to_string()),
+        literal: lit,
     }
 }
 
+fn keywords() -> HashMap<String,TokenType>  {
+    [
+        (String::from("fn"), TokenType::FUNCTION),
+        (String::from("let"), TokenType::LET),
+    ]
+    .iter()
+    .cloned()
+    .collect()
+}
 impl Lexer {
+
+    fn read_identifier(&mut self ) -> &str{
+        let st = self.position;
+        while self.ch.is_alphabetic() {
+            self.read_char();
+        }
+        &self.input[st..self.position]
+    }
+
     fn read_char(&mut self) {
         self.ch = if self.readPosition >= self.input.len() {
-            0 as char
+            '\0'
         } else {
             self.input.to_string().as_bytes()[self.readPosition] as char
         };
@@ -48,17 +67,24 @@ impl Lexer {
         self.position = self.readPosition;
         self.readPosition += 1;
     }
+
     fn next_token(&mut self) -> Token {
         let tok = match self.ch {
-            '=' => cons_token(TokenType::ASSIGN, self.ch),
-            ';' => cons_token(TokenType::SEMICOLON, self.ch),
-            '(' => cons_token(TokenType::LPAREN, self.ch),
-            ')' => cons_token(TokenType::RPAREN, self.ch),
-            ',' => cons_token(TokenType::COMMA, self.ch),
-            '+' => cons_token(TokenType::PLUS, self.ch),
-            '{' => cons_token(TokenType::LBRACE, self.ch),
-            '}' => cons_token(TokenType::RBRACE, self.ch),
-            _ => cons_token(TokenType::EOF, '\0'),
+            '=' => cons_token(TokenType::ASSIGN, self.ch.to_string()),
+            ';' => cons_token(TokenType::SEMICOLON, self.ch.to_string()),
+            '(' => cons_token(TokenType::LPAREN, self.ch.to_string()),
+            ')' => cons_token(TokenType::RPAREN, self.ch.to_string()),
+            ',' => cons_token(TokenType::COMMA, self.ch.to_string()),
+            '+' => cons_token(TokenType::PLUS, self.ch.to_string()),
+            '{' => cons_token(TokenType::LBRACE, self.ch.to_string()),
+            '}' => cons_token(TokenType::RBRACE, self.ch.to_string()),
+            '\0' => cons_token(TokenType::EOF, '\0'.to_string()),
+            chr => 
+                if chr.is_alphabetic() || chr == '_' {
+                    cons_token(keywords().get(&chr.to_string()).unwrap().clone(), String::from(self.read_identifier()))
+                } else {
+                    cons_token(TokenType::ILLEGAL,chr.to_string())
+                }
         };
         self.read_char();
         return tok;
@@ -75,6 +101,7 @@ fn lexer_of_str(string: &str) -> Lexer {
     ret_lex.read_char();
     ret_lex
 }
+
 fn lex(sample_input: &str) -> std::vec::Vec<Token> {
     let mut lexer = lexer_of_str(sample_input);
     let mut tokens = Vec::new();
@@ -270,6 +297,7 @@ fn test_case_two_vec() -> std::vec::Vec<Token> {
         },
     ]
 }
+
 fn main() {
     println!("Hello, world!");
 }
@@ -279,6 +307,18 @@ mod tests {
     #[test]
     fn simple_test() {
         let sample_input = String::from("=+(){},;");
-        assert_eq!(super::test_case_one_vec(), super::lex("=+(){},;"));
+        assert_eq!(super::test_case_one_vec(), super::lex("=+(){},;"))
+    }
+   #[test] 
+    fn test_1() {
+     let input = "let five = 5;
+     let ten = 10;
+     let add = fn(x, y) {
+    x + y; 
+    };
+    
+    let result = add(five,ten); ";
+
+    assert_eq!(super::test_case_two_vec(),super::lex(input));
     }
 }
